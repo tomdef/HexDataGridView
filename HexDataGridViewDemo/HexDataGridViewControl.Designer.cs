d6a328784aa9e33f22142dde185f7dbc29caae97
tomdef <tomdef@wp.pl>
2022-10-09 14:46:11 +0200

    fixes

--- HexDataGridViewDemo/HexDataGridViewControl.Designer.cs
+++ HexDataGridViewDemo/HexDataGridViewControl.Designer.cs
@@ -28,7 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            //System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            //System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.SuspendLayout();
@@ -41,15 +42,24 @@
             this.dataGridView.AllowUserToResizeRows = false;
             this.dataGridView.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            //dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            //dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            //dataGridViewCellStyle1.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            //dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            //dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            //dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            //dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            //this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle1;
+            //dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            //dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            //dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            //dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            //dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            //dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            //dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            //this.dataGridView.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView.GridColor = System.Drawing.SystemColors.Menu;
             this.dataGridView.Location = new System.Drawing.Point(0, 0);
@@ -60,12 +70,12 @@
             this.dataGridView.Size = new System.Drawing.Size(457, 206);
             this.dataGridView.TabIndex = 1;
             // 
-            // BytesEditControl
+            // HexDataGridViewControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.dataGridView);
-            this.Name = "BytesEditControl";
+            this.Name = "HexDataGridViewControl";
             this.Size = new System.Drawing.Size(457, 206);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
--- HexDataGridViewDemo/HexDataGridViewControl.cs
+++ HexDataGridViewDemo/HexDataGridViewControl.cs
@@ -17,59 +17,39 @@ namespace WinFormsTest
         private Func<byte, string> byteToString;
         private Func<string, byte> stringToByte;
 
+        private readonly Font defaultFont = new Font(FontFamily.GenericMonospace, 9);
+        private DataGridViewCellStyle defaultCellStyle;
+        private DataGridViewCellStyle defaultHexCellStyle;
+        private DataGridViewCellStyle defaultAsciiCellStyle;
+        private DataGridViewCellStyle defaultHeaderCellStyle;
+        private Size cellSize;
+
+        private DataGridViewRow rowHex;
+        private DataGridViewRow rowAscii;
+
+        private StringFormat sf = new StringFormat()
+        {
+            Alignment = StringAlignment.Center,
+            LineAlignment = StringAlignment.Center,
+        };
+
         public Encoding DefaultEncoding { get; set; } = Encoding.GetEncoding(1250);
 
         public HexDataGridViewControl()
         {
             InitializeComponent();
+
+            InitializeGridViewStyle();
             InitializeEvents();
+
 
             byteToHexString = ByteToHexString;
             byteToString = ByteToString;
             stringToByte = StringToByte;
-            Padding = new Padding(5);
         }
 
         public void SetupGridView(byte[] data)
         {
-            dataGridView.AutoGenerateColumns = false;
-            dataGridView.RowHeadersVisible = false;
-
-            DataGridViewCellStyle defaultCellStyle = new DataGridViewCellStyle(dataGridView.DefaultCellStyle)
-            {
-                Alignment = DataGridViewContentAlignment.MiddleCenter,
-                Font = new Font(FontFamily.GenericMonospace, 9)
-            };
-
-            DataGridViewCellStyle defaultHeaderCell = new DataGridViewCellStyle()
-            {
-                Alignment = DataGridViewContentAlignment.MiddleCenter,
-                Font = new Font(FontFamily.GenericMonospace, 9),
-                BackColor  = SystemColors.ActiveCaption,
-                ForeColor = SystemColors.ActiveCaptionText,
-            };
-
-            Size cellSize = TextRenderer.MeasureText(charToMeasure, defaultCellStyle.Font);
-            cellSize.Width += 8;
-            cellSize.Height += 8;
-
-            DataGridViewRow rowHex = new DataGridViewRow()
-            {
-                DefaultCellStyle = defaultCellStyle,
-                Height = cellSize.Height
-            };
-
-            DataGridViewRow rowAscii = new DataGridViewRow()
-            {
-                DefaultCellStyle = defaultCellStyle,
-                Height = cellSize.Height
-            };
-
-            // column style
-            dataGridView.ColumnHeadersHeight = (cellSize.Height * 3);
-
-            // fill
-
             int index = 0;
             foreach (byte b in data)
             {
@@ -89,12 +69,14 @@ namespace WinFormsTest
                 {
                     Value = b,
                     MaxInputLength = 2,
+                    Style = defaultHexCellStyle
                 });
 
                 rowAscii.Cells.Add(new DataGridViewTextBoxCell()
                 {
                     Value = b,
-                    MaxInputLength = 1
+                    MaxInputLength = 1,
+                    Style = defaultAsciiCellStyle
                 });
             }
 
@@ -102,11 +84,79 @@ namespace WinFormsTest
             dataGridView.Rows.Add(rowAscii);
         }
 
+        private void InitializeGridViewStyle()
+        {
+            dataGridView.AutoGenerateColumns = false;
+            dataGridView.RowHeadersVisible = false;
+
+            defaultCellStyle = new DataGridViewCellStyle(dataGridView.DefaultCellStyle)
+            {
+                Alignment = DataGridViewContentAlignment.MiddleCenter,
+                Font = defaultFont
+            };
+
+            defaultHexCellStyle = new DataGridViewCellStyle(defaultCellStyle)
+            {
+                BackColor = SystemColors.Info,
+                ForeColor = SystemColors.InfoText
+            };
+
+            defaultAsciiCellStyle = new DataGridViewCellStyle(defaultCellStyle);
+
+            defaultHeaderCellStyle = new DataGridViewCellStyle(defaultCellStyle)
+            {
+                BackColor = SystemColors.ActiveCaption,
+                ForeColor = SystemColors.ActiveCaptionText,
+            };
+
+            cellSize = TextRenderer.MeasureText(charToMeasure, defaultCellStyle.Font);
+            cellSize.Width += 8;
+            cellSize.Height += 8;
+
+            rowHex = new DataGridViewRow()
+            {
+                DefaultCellStyle = defaultHexCellStyle,
+                Height = cellSize.Height * 2
+            };
+
+            rowAscii = new DataGridViewRow()
+            {
+                DefaultCellStyle = defaultAsciiCellStyle,
+                Height = cellSize.Height
+            };
+
+            // column style
+            dataGridView.ColumnHeadersHeight = (cellSize.Height * 3);
+        }
+
         private void InitializeEvents()
         {
             dataGridView.CellParsing += CellParsing;
             dataGridView.CellValidating += CellValidating;
             dataGridView.CellFormatting += CellFormatting;
+
+            dataGridView.CellPainting += CellPainting;
+        }
+
+        // -------------------------------------------------------------------------
+
+        #region DataGridView Events
+
+        private void CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            if (e.RowIndex == 0)
+            {
+                DrawHexCell(e);
+                e.Handled = true;
+            }
+            else
+            {
+                if (e.RowIndex == -1)
+                {
+                    DrawHeaderCell(e);
+                    e.Handled = true;
+                }
+            }
         }
 
         private void CellParsing(object sender, DataGridViewCellParsingEventArgs e)
@@ -168,6 +218,77 @@ namespace WinFormsTest
             }
         }
 
+        #endregion DataGridView Events
+
+
+        #region DataGridView Draw
+
+        private void DrawHexCell(DataGridViewCellPaintingEventArgs e)
+        {
+            var val = e.FormattedValue.ToString();
+
+            if (val.Length == 2)
+            {
+                bool isSelected = (e.State & DataGridViewElementStates.Selected) == DataGridViewElementStates.Selected;
+                e.PaintBackground(e.CellBounds, isSelected);
+                RectangleF backRect = new RectangleF(e.CellBounds.X + 2, e.CellBounds.Y + 2, e.CellBounds.Width - 4, e.CellBounds.Height - 4);
+                //e.Graphics.FillRectangle(new SolidBrush(defaultHexBackColor), backRect);
+
+
+                SizeF charSize = e.Graphics.MeasureString(charToMeasure, dataGridView.DefaultCellStyle.Font);
+                float cellHalfHeight = e.CellBounds.Height / 2;
+                int m = 0;
+
+                PointF p1 = new PointF(e.CellBounds.X + m, e.CellBounds.Y + m);
+                PointF p2 = new PointF(e.CellBounds.X + m, e.CellBounds.Y + m + charSize.Height);
+
+                RectangleF r1 = new RectangleF(p1, new SizeF(e.CellBounds.Width, cellHalfHeight));
+                RectangleF r2 = new RectangleF(p2, new SizeF(e.CellBounds.Width, cellHalfHeight));
+
+                e.Graphics.DrawString(
+                    val[0].ToString(), defaultFont, new SolidBrush(SystemColors.ControlText), r1, sf);
+
+                e.Graphics.DrawString(
+                    val[1].ToString(), defaultFont, new SolidBrush(SystemColors.ControlText), r2, sf);
+            }
+        }
+
+        private void DrawHeaderCell(DataGridViewCellPaintingEventArgs e)
+        {
+            var val = e.FormattedValue.ToString();
+
+            if (val.Length > 0)
+            {
+                bool isSelected = (e.State & DataGridViewElementStates.Selected) == DataGridViewElementStates.Selected;
+                e.PaintBackground(e.CellBounds, isSelected);
+                RectangleF backRect = new RectangleF(e.CellBounds.X + 2, e.CellBounds.Y + 2, e.CellBounds.Width - 4, e.CellBounds.Height - 4);
+                e.Graphics.FillRectangle(new SolidBrush(defaultHeaderCellStyle.BackColor), backRect);
+
+                int m = 2;
+                SizeF charSize = e.Graphics.MeasureString(charToMeasure, dataGridView.DefaultCellStyle.Font);
+                float cellCharHeight = (e.CellBounds.Height - (val.Length * m)) / (val.Length);
+                
+
+                PointF p = new PointF(e.CellBounds.X, e.CellBounds.Y + (m * 2));
+
+                for (int i = 0; i < val.Length; i++)
+                {
+                    RectangleF r = new RectangleF(p, new SizeF(e.CellBounds.Width, cellCharHeight));
+
+                    e.Graphics.DrawString(
+                        val[i].ToString(), defaultHeaderCellStyle.Font, new SolidBrush(defaultHeaderCellStyle.ForeColor), r, sf);
+
+
+                    p = new PointF(p.X, p.Y + cellCharHeight);
+                    //p.Y += p.Y + 10;// cellCharHeight;
+                }
+            }
+        }
+
+        #endregion DataGridView Draw
+
+        #region Private methods
+
         private void UpdateAsciiValue(int columnIndex, byte value)
         {
             dataGridView.CellParsing -= CellParsing;
@@ -181,6 +302,8 @@ namespace WinFormsTest
             dataGridView.Rows[rowIndexHex].Cells[columnIndex].Value = value;
             dataGridView.CellParsing += CellParsing;
         }
+
+        #endregion Private methods
 
         #region DefaultConversions
 
